bc422deab0ef114608cc8286a33d2b1d5ac030b3
Zhenya Peteliev <petelev.zhenya@gmail.com>
2019-04-08 22:26:31 +0300

    Implement basic project structure.

---
+++ Example/YPSwipeableStackViewExample/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YPSwipeableStackViewExample
+//
+//  Created by Zhenya Peteliev on 4/8/19.
+//  Copyright © 2019 Yevhenii Peteliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YPSwipeableStackView/YPSwipeableStackView/YPSwipeableStackView.h
@@ -0,0 +1,19 @@
+//
+//  YPSwipeableStackView.h
+//  YPSwipeableStackView
+//
+//  Created by Zhenya Peteliev on 4/8/19.
+//  Copyright © 2019 Yevhenii Peteliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YPSwipeableStackView.
+FOUNDATION_EXPORT double YPSwipeableStackViewVersionNumber;
+
+//! Project version string for YPSwipeableStackView.
+FOUNDATION_EXPORT const unsigned char YPSwipeableStackViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YPSwipeableStackView/PublicHeader.h>
+
+
